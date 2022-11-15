9af91cfe62f5e944849dd0e79cc9496fe05399cd
RajputHim <105914405+RajputHim@users.noreply.github.com>
2022-11-15 18:38:27 +0530

    Added java project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let slideIndex = 0;
 let slideIndex2 = 0;
+let slideIndex3 = 0;
 showSlides();
 showSlides2();
+showSlides3();
 
 function showSlides() {
   let i;
@@ -34,4 +36,20 @@ function showSlides2() {
   slides[slideIndex2 - 1].style.display = "block";
 
   setTimeout(showSlides2, 3000); // Change image every 3 seconds
+}
+function showSlides3() {
+  let i;
+  let slides = document.getElementsByClassName("slide3");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex3++;
+  if (slideIndex3 > slides.length) {
+    slideIndex3 = 1;
+  }
+
+  slides[slideIndex3 - 1].style.display = "block";
+
+  setTimeout(showSlides3, 3000); // Change image every 3 seconds
 }
